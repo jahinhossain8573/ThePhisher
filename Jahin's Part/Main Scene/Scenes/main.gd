482488f24f5f4d60d161c93236755134e6a02b5b
Md Jahin Hossain <jahin.hossain2006@gmail.com>
2026-08-16 17:14:04 +0600

    Added clickbait to the end of the spline

--- Jahin's Part/Main Scene/Scenes/main.gd
+++ Jahin's Part/Main Scene/Scenes/main.gd
@@ -5,12 +5,16 @@ extends Node3D
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		$Path3D.curve.set_point_position(2, Vector3.UP * reeling_speed + $Path3D.curve.get_point_position(2))
+		$Path3D/PathFollow3D.progress_ratio = 1
 	
 	if Input.is_action_pressed("move_down"):
 		$Path3D.curve.set_point_position(2, Vector3.DOWN * reeling_speed + $Path3D.curve.get_point_position(2))
-	
+		$Path3D/PathFollow3D.progress_ratio = 1
+		
 	if Input.is_action_pressed("move_left"):
 		$Path3D.curve.set_point_position(2, Vector3.LEFT * reeling_speed + $Path3D.curve.get_point_position(2))
-	
+		$Path3D/PathFollow3D.progress_ratio = 1
+		
 	if Input.is_action_pressed("move_right"):
 		$Path3D.curve.set_point_position(2, Vector3.RIGHT * reeling_speed + $Path3D.curve.get_point_position(2))
+		$Path3D/PathFollow3D.progress_ratio = 1
