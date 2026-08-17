3ffee155bcaee95ead6146788bfdf12f99583dcc
Md Jahin Hossain <jahin.hossain2006@gmail.com>
2026-08-17 18:34:24 +0600

    Added a way to disable and enable QTEs

--- Jahin's Part/Secondary Scenes/qte.gd
+++ Jahin's Part/Secondary Scenes/qte.gd
@@ -3,16 +3,19 @@ extends Control
 signal QuickTimeSuccess
 signal QuickTimeFailure
 
-#Slider Speed
+# Slider Speed
 @export var slider_speed : float = 2.5
 
-#Declaring markers as variables
+# Minigame Status
+var enabled : bool = false
+
+# Declaring markers as variables
 @onready var start_marker: Marker2D = $Panel/StartMarker
 @onready var end_marker: Marker2D = $Panel/EndMarker
 @onready var trigger_zone_start_marker: Marker2D = $Panel/TriggerZoneStartMarker
 @onready var trigger_zone_end_marker: Marker2D = $Panel/TriggerZoneEndMarker
 
-#Declaring Panel and Slider as variables
+# Declaring Panel and Slider as variables
 @onready var panel: Panel = $Panel
 @onready var slider_bar: Sprite2D = $Panel/SliderBar
 
@@ -20,24 +23,34 @@ signal QuickTimeFailure
 var is_moving_right : bool = true
 
 func _ready() -> void:
-	pass # Replace with function body.
+	disable()
 
 
 func _physics_process(delta: float) -> void:
-	if slider_bar.global_position.x >= end_marker.global_position.x:
-		is_moving_right = false
-	elif slider_bar.global_position.x <= start_marker.global_position.x:
-		is_moving_right = true
+	if enabled:
+		if slider_bar.global_position.x >= end_marker.global_position.x:
+			is_moving_right = false
+		elif slider_bar.global_position.x <= start_marker.global_position.x:
+			is_moving_right = true
 		
-	if is_moving_right:
-		slider_bar.global_position.x += slider_speed
-	else:
-		slider_bar.global_position.x -= slider_speed
-	
-	if Input.is_action_just_pressed("interaction"):
-		if slider_bar.global_position.x >= trigger_zone_start_marker.global_position.x and slider_bar.global_position.x <= trigger_zone_end_marker.global_position.x:
-			print("Superb")
-			QuickTimeSuccess.emit()
+		if is_moving_right:
+			slider_bar.global_position.x += slider_speed
 		else:
-			print("Wrong Click")
-			QuickTimeFailure.emit()
+			slider_bar.global_position.x -= slider_speed
+	
+		if Input.is_action_just_pressed("interaction"):
+			if slider_bar.global_position.x >= trigger_zone_start_marker.global_position.x and slider_bar.global_position.x <= trigger_zone_end_marker.global_position.x:
+				print("Superb")
+				QuickTimeSuccess.emit()
+			else:
+				print("Wrong Click")
+				QuickTimeFailure.emit()
+
+	
+func enable():
+	visible = true
+	enabled = true
+
+func disable():
+	visible = false
+	enabled = false
