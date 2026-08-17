83d40a2368f7542bd4484b9a29332a769d831476
Md Jahin Hossain <jahin.hossain2006@gmail.com>
2026-08-17 12:06:05 +0600

    Added in QuickTime functionality

--- Jahin's Part/Secondary Scenes/qte.gd
+++ Jahin's Part/Secondary Scenes/qte.gd
@@ -30,3 +30,7 @@ func _physics_process(delta: float) -> void:
 		slider_bar.global_position.x += slider_speed
 	else:
 		slider_bar.global_position.x -= slider_speed
+	
+	if Input.is_action_just_pressed("interaction"):
+		if  slider_bar.global_position.x >= trigger_zone_start_marker.global_position.x and slider_bar.global_position.x <= trigger_zone_end_marker.global_position.x:
+			print("Superb")
