cbba0223652dd170ca1025ca048ccd1ce1fa4741
Md Jahin Hossain <jahin.hossain2006@gmail.com>
2026-08-19 10:23:15 +0600

    Added in a UI to display the score and the high score

---
+++ Jahin's Part/Secondary Scenes/UI/gameplay_ui.gd
@@ -0,0 +1,15 @@
+extends Control
+
+class_name GameplayUI
+
+@onready var score: Label = $VBoxContainer/Score
+@onready var high_score: Label = $"VBoxContainer/High Score"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
