cecbfdb18544e0e5567ecf5226f74830b1315e4d
Md Jahin Hossain <jahin.hossain2006@gmail.com>
2026-08-20 22:06:28 +0600

    Added in a bunch of stuff

--- Jahin's Part/Main Scene/Scenes/main.gd
+++ Jahin's Part/Main Scene/Scenes/main.gd
@@ -18,6 +18,7 @@ var input_accept : bool = true
 @onready var gameplay_ui: GameplayUI = $"Camera3D/UI Manager/Sprite3D/SubViewport/GameplayUI"
 
 @onready var session_timeout_timer: Timer = $SessionTimer/SessionTimeoutTimer
+@onready var gameplay_ui_1: HighScoreDisplay = $"Camera3D/UI Manager/Sprite3D1/SubViewport/GameplayUI1"
 
 
 func _ready() -> void:
@@ -29,7 +30,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$Path3D/PathFollow3D/Clickbait/Minigame.global_rotation.y = 0
 	$Path3D/PathFollow3D/Clickbait/Minigame.global_position.z = clickbait.global_position.z + 0.4
-	gameplay_ui.label.text = "Score: " + str(score) + "\nHigh Score: " + str(GameManager.high_score)
+	gameplay_ui.label.text = "Score: " + str(score)
+	gameplay_ui_1.label.text = "High Score: " + str(GameManager.high_score)
+	
+	$TimeLeft.text = "Time Left: %.2f" % $SessionTimer.time_left
 
 func _physics_process(delta: float) -> void:
 	#Movement
@@ -38,18 +42,30 @@ func _physics_process(delta: float) -> void:
 			if $Path3D/PathFollow3D/Clickbait/Minigame/SubViewport/QTE.enabled == false:
 				if Input.is_action_pressed("move_up"):
 					$Path3D.curve.set_point_position(2, Vector3.FORWARD * reeling_speed + $Path3D.curve.get_point_position(2))
+					var position = $Path3D.curve.get_point_position(2)
+					position.z = clamp(position.z, -1.118, 5.926)
+					$Path3D.curve.set_point_position(2, position)
 					$Path3D/PathFollow3D.progress_ratio = 1
 				
 				if Input.is_action_pressed("move_down"):
 					$Path3D.curve.set_point_position(2, Vector3.BACK * reeling_speed + $Path3D.curve.get_point_position(2))
+					var position = $Path3D.curve.get_point_position(2)
+					position.z = clamp(position.z, -1.118, 5.926)
+					$Path3D.curve.set_point_position(2, position)
 					$Path3D/PathFollow3D.progress_ratio = 1
 					
 				if Input.is_action_pressed("move_left"):
 					$Path3D.curve.set_point_position(2, Vector3.LEFT * reeling_speed + $Path3D.curve.get_point_position(2))
+					var position = $Path3D.curve.get_point_position(2)
+					position.x = clamp(position.x, -8.844, 8.844)
+					$Path3D.curve.set_point_position(2, position)
 					$Path3D/PathFollow3D.progress_ratio = 1
 					
 				if Input.is_action_pressed("move_right"):
 					$Path3D.curve.set_point_position(2, Vector3.RIGHT * reeling_speed + $Path3D.curve.get_point_position(2))
+					var position = $Path3D.curve.get_point_position(2)
+					position.x = clamp(position.x, -8.844, 8.844)
+					$Path3D.curve.set_point_position(2, position)
 					$Path3D/PathFollow3D.progress_ratio = 1
 		else:
 			if Input.is_action_just_pressed("move_up"):
@@ -64,6 +80,7 @@ func _physics_process(delta: float) -> void:
 				if clickbait.type <= 0:
 					clickbait.type = 5
 				else:
+					
 					clickbait.type -= 1
 				clickbait.type_change()
 				#print(clickbait.type)
@@ -130,6 +147,7 @@ func _on_session_timer_timeout() -> void:
 	input_accept = false
 	session_timeout_timer.start()
 	$AnimationPlayer.play("Session Timeout")
+	$"Camera3D/UI Manager/Sprite3D4/SubViewport/SessionEnd".label.text = "Score: " + str(score) + "\nHigh Score: " + str(GameManager.high_score)
 
 func _on_session_timeout_timer_timeout() -> void:
 	input_accept = true
